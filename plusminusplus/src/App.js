49c138f4f982c98e0cb32404460eabf841bb885a
derenyilmaz <3rendeniz@gmail.com>
2024-01-13 23:16:43 +0300

    feat: add image rescaling and background recoloring. and add placeholder image so as not to exceed the api request limit

--- plusminusplus/src/App.js
+++ plusminusplus/src/App.js
@@ -3,6 +3,47 @@ import React, { useEffect, useState } from "react";
 
 const imageUrl = "https://api.unsplash.com/photos/random/?client_id=" + process.env.REACT_APP_ACCESS_KEY;
 
+const sendRequest = false;
+const percentage = 80;
+
+var color = "#355e3b";
+
+
+window.onresize = window.onload = function()
+{
+  resize();
+}
+
+function getWindowDimensions()
+{
+  var body = document.documentElement || document.body;
+  
+  return {
+    x: window.innerWidth  || body.clientWidth,
+    y: window.innerHeight || body.clientHeight
+  }
+}
+
+function resize()
+{
+  // there must be a better way than getting the image element by tag name
+  var img = document.getElementsByTagName('img')[0];
+  var windowDimensions = getWindowDimensions();
+  
+  img.style.height = windowDimensions.y * (percentage/100) + "px";
+
+  if (img.offsetWidth > windowDimensions.x)
+  {
+      img.style.height = null;
+      img.style.width = windowDimensions.x * (percentage/100) + "px";
+  }
+}
+
+function recolor()
+{
+  document.body.style.backgroundColor = color;
+}
+
 function App() {
   const [img, setImg] = useState();
 
@@ -13,12 +54,23 @@ function App() {
     })
     .then(data => {
       setImg(data.urls.regular);
+      color = data.color.toString();
     });
-
   };
 
+  const showStaticImage = () => {
+    const img = require('./placeholder.png');
+    setImg(img);
+  }
+
   useEffect(() => {
-    fetchImage();
+    if (sendRequest){
+      fetchImage();
+    }
+    else {
+      showStaticImage();
+    }
+    recolor();
   }, []);
 
   return (
